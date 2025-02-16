b7b628060b4646dfd6e40249926bd0e9ae0b30ca
Dmtiry Burmashkin <144222958+DmitryBurmashkin@users.noreply.github.com>
2025-02-16 22:10:14 +0300

    task_2.py

--- Лабораторная работа 2/task_2.py
+++ Лабораторная работа 2/task_2.py
@@ -12,7 +12,6 @@ BOOKS_DATABASE = [
 ]
 
 
-# TODO: написать класс Book
 class Book:
     def __init__(self, id_: int, name: str, pages: int):
         self.id = id_
@@ -25,18 +24,18 @@ class Book:
     def __repr__(self):
         return f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
 
-# TODO: написать класс Library
+
 class Library:
     def __init__(self, books=None):
         self.books = books if books is not None else []
 
     def get_next_book_id(self):
-        if not self.books:  # Если список книг пуст
+        if not self.books:
             return 1
-        return self.books[-1].id + 1  # Возвращаем id последней книги + 1
+        return self.books[-1].id + 1
 
     def get_index_by_book_id(self, book_id):
-        for index, book in enumerate(self.books):  # Используем enumerate для получения пар индекс-значение
+        for index, book in enumerate(self.books):
             if book.id == book_id:
                 return index
         raise ValueError("Книги с запрашиваемым id не существует")
@@ -46,13 +45,14 @@ class Library:
 
 
 if __name__ == '__main__':
-    empty_library = Library()  # инициализируем пустую библиотеку
-    print(empty_library.get_next_book_id())  # проверяем следующий id для пустой библиотеки
+    empty_library = Library()
+    print(empty_library.get_next_book_id())
 
     list_books = [
-        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"])
+        for book_dict in BOOKS_DATABASE
     ]
-    library_with_books = Library(books=list_books)  # инициализируем библиотеку с книгами
-    print(library_with_books.get_next_book_id())  # проверяем следующий id для непустой библиотеки
+    library_with_books = Library(books=list_books)
+    print(library_with_books.get_next_book_id())
 
-    print(library_with_books.get_index_by_book_id(1))  # проверяем индекс книги с id = 1
+    print(library_with_books.get_index_by_book_id(1))
